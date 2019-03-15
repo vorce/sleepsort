62ef180f722f0ab000051cd49a0a38d06fe2eacf
Joel Carlbark <joel.carlbark@meltwater.com>
2019-03-15 16:09:10 +0100

    Add dialyzer config and fix caching in circleci

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Sleepsort.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
